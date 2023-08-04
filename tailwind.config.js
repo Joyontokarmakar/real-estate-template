66a5c8f760503c3cd4206fa2832034907a50dca5
Joyonto Karmakar <joyonto1997@gmail.com>
2023-08-05 00:41:49 +0600

    design done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,12 @@ export default {
         lightColor: '#FEFCF8',
         lightColor2: '#FEFBF8',
         lightColor3: '#FAF1EB',
+        lightColor4: '#FFF5EC',
         darkGrayColor: '#282A2E',
         grayColor: '#ADB1BA',
         grayColor2: '#606878',
+        redColor: '#E16E4F',
+        redColor2: '#FAEFE5'
       },
       backgroundImage: {
         'hero-pattern': "url('/images/hero-banner.png')",
