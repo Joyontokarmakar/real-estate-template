a60e567c0628068e72b3fc73185addcaee74e1f9
Joyonto Karmakar <joyonto1997@gmail.com>
2023-08-05 23:54:36 +0600

    font initialize done

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ export default {
       boxShadow: {
         'sliderShadow': '-5px 0px 34px 0px #D7D7D7;',
       },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      },
       screens: {
         rotateMobile: { min: "515px", max: "574px" },
         tablet: { min: "575px", max: "639px" },
